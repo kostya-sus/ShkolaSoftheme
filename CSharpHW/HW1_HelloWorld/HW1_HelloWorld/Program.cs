ab85c679bfd6197f77753fe1aafe25c401d97752
kostya-sus <kostya-sus@yandex.ru>
2016-10-19 23:34:00 +0300

    Base implimentation of HomeWork_1

---
+++ CSharpHW/HW1_HelloWorld/HW1_HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HW1_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "Kostya";
+
+            System.Console.ForegroundColor = ConsoleColor.Yellow; 
+
+            System.Console.WriteLine("Hello {0}", name);
+            System.Console.ReadKey();
+        }
+    }
+}
